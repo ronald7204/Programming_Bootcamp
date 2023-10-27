dcbeb2040c0fffd36a2fd090292c94901fc9d9d1
ronald7204 <ronabys@gmail.com>
2023-10-27 21:39:22 +1100

    comments

--- Task2.py
+++ Task2.py
@@ -10,31 +10,27 @@ import re
 from random import randint
 
 def generate_number():
-    target = randint(1, 100)
+    target = randint(1, 100) #generates a random integer between [1, 100] inclusive
     return target
 
 
-def validate_input(target:int):
+def validate_input(target:int): 
     guess = input("Please enter an integer between [1,100]: ")
-    if re.search(r"\d+", guess):
+    if re.search(r"\d+", guess): #re (regular expression) function that makes sure the input ONLY contains integer values
         guess = int(guess)
         if guess >= 1 and guess <= 100:
-            # print("yay") #assuming that 1 and 100 are not included since "between"
-            # return guess
             if guess == target: 
-                return "yay"
+                return "yay" 
             elif guess > target:
                 return "lower"
             elif guess < target: 
                 return "higher"
             """include code for comparing user input with target number"""
         else: 
-            # print("please input an integer between 1 and 100")
-            # return validate_input()
+            
             return "try again2"
     else: 
-        # print("please input an integer")
-        # return validate_input()
+        
         return "try again1"
 
 def print_message():
@@ -44,9 +40,10 @@ def print_message():
     2. higher, lower, correct
 
     """
-    target = generate_number()
-    while True:
-        user_input = validate_input(target) 
+    target = generate_number() #generates a target number and assigns it to a variable called target
+    while True: #repeats the process of validating input/guessing until the right number is guessed
+        user_input = validate_input(target) #validate input is called and the value returned is assigned to user_input
+        #the following conditional statments correspond to the validate_input function 
         if user_input == "try again2":
             print("please input an integer")
         elif user_input == "try again1":
@@ -57,7 +54,7 @@ def print_message():
             print("LOWER!")
         else: 
             print("correct!\nThankyou for playing! Have a nice day :)")
-            return False
+            return False #when the right number is guessed the program completes
 
 
     
